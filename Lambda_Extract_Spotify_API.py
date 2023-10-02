4b69c8db0d898a242c1e5a6ebbab19404ee0d091
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2023-10-02 14:11:34 -0500

    Update Lambda_Extract_Spotify_API.py

--- Lambda_Extract_Spotify_API.py
+++ Lambda_Extract_Spotify_API.py
@@ -14,10 +14,40 @@ def lambda_handler(event, context):
     sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
     playlists = sp.user_playlists('spotify')
     
-    playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+    playlist_link = "https://open.spotify.com/playlist/2YRe7HRKNRvXdJBp9nXFza"
     playlist_URI = playlist_link.split("/")[-1].split("?")[0]
     
-    spotify_data = sp.playlist_tracks(playlist_URI)   
+    # Initialize an empty list to store all the tracks
+    all_tracks = []
+    
+    # Initialize the offset to 0
+    offset = 0
+    
+    # Set the limit to the maximum allowed (100 tracks per request)
+    limit = 100
+    
+    while True:
+        # Retrieve the next batch of tracks
+        batch = sp.playlist_tracks(playlist_URI, offset=offset, limit=limit)
+    
+        # Add the batch of tracks to the list
+        all_tracks.extend(batch['items'])
+    
+        # Check if there are more tracks to retrieve
+        if len(batch['items']) < limit:
+            break
+    
+        # Increment the offset for the next request
+        offset += limit
+    
+    # Continue with the rest of your code to process the data
+    
+    # The code that follows can remain the same as in your original code
+    spotify_data = {
+        'href': batch['href'],  # The last href from the loop
+        'items': all_tracks
+    }
+    # spotify_data = sp.playlist_tracks(playlist_URI)   
     cilent = boto3.client('s3')
     
     filename = "spotify_raw_" + str(datetime.now()) + ".json"
